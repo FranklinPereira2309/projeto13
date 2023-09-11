8945cb044ad33c917cb205ed4d336649b96d5206
Franklin Menezes <fmpereira2309@hotmail.com>
2023-09-11 17:43:35 -0300

    iniciando exercicio

---
+++ exercicio1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct passageiro
+{
+    string nome;
+    string cpf;
+    int idade;
+
+};
+
+struct passagem {
+    float valor;
+    int assento;
+
+};
+
+struct data_hora {
+    int dia;
+    int mes;
+    int ano;
+    int hora;
+};
+
+int main() {
+    return 0;
+}
+
+
