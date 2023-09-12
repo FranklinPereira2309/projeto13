fbe9704712485e852b30ee147df50474b40c8e3e
Franklin Menezes <fmpereira2309@hotmail.com>
2023-09-12 16:09:07 -0300

    criação da estrutrua inicial

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -1,32 +1,76 @@
 #include <iostream>
+#include <iomanip>
+#include <vector>
 
 using namespace std;
 
-struct Passageiro
-{
+// struct Passageiro
+// {
+//     string nome;
+//     string cpf;
+//     int idade;
+
+// };
+
+struct Passagem {
+    float valor = 80;
+    int assento;
     string nome;
     string cpf;
     int idade;
-
-};
-
-struct Passagem {
-    float valor;
-    int assento;
-
-};
-
-struct Data_Hora {
     int dia;
     int mes;
-    int ano;
+    int ano = 365;
     int hora;
+    string tipoViagem;
+
 };
 
+// struct Data_Hora {
+// };
+
 int main() {
-    Passageiro p1;
-    Passagem p1;
-    Data_Hora p1;
+    vector<Passagem> viagens;
+    Passagem novaPassagem;
+
+    string tempString;
+    bool continuar = true;
+
+    float valorObtido = 0, qtdLugares = 0;
+    int sair = 1;
+    int conte;
+    
+        
+     while(continuar) {
+                cout << "Digite seu nome: ";
+                getline(cin >> ws,novaPassagem.nome);
+                cout << "Digite seu cpf: ";
+                getline(cin >> ws, novaPassagem.cpf);
+                cout << "Digite o dia: ";
+                cin >> novaPassagem.dia;
+                cout << "Digite o mes: ";
+                cin >> novaPassagem.mes;
+                cout << "Digite tipo de percurso (IDA/VOLTA): ";
+                cin >> novaPassagem.tipoViagem;
+                cout << "Digite o horario: ";
+                cin >> novaPassagem.hora;
+                cout << "Continuar(s/n)? ";
+                viagens.push_back(novaPassagem);
+                getline(cin >> ws, tempString);
+                continuar = tempString == "s"? true : false;
+    }
+        
+               
+    
+
+    // for(int i = 0 ; i < conte; i++) {
+            
+    //     }
+
+    
+    
+
+
     
     return 0;
 }
